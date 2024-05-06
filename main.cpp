99e8d4cebb537d2647f5d88052a5c683e48d9edb
Alex Mathew <115664488+alxthe@users.noreply.github.com>
2024-05-06 14:20:33 +0000

    edited print statements

--- main.cpp
+++ main.cpp
@@ -8,5 +8,5 @@ int main() {
   cout << "WELCOME TO LASA VEGAS! READY TO PLAY SOME POKER? LET'S GET STARTED :)\n\n";
   cout << "How much do you want to bet (in dollars)?\n";
   cin >> x;
-  cout << "Okay, you bet " << x << " dollars.";
+  cout << "\nOkay, you bet " << x << " dollars.";
 }
